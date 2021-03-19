485677509cf153ef31e760baf0768006281cbd59
musiermoore <musiermoore@gmail.com>
2021-03-19 15:12:34 +0600

    Created models, migrations and controllers for student rate

---
+++ app/Http/Controllers/Api/RateController.php
@@ -0,0 +1,19 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+
+class RateController extends Controller
+{
+    public function setRateStudent($id)
+    {
+
+    }
+
+    public function getRatesStudents($group_id)
+    {
+
+    }
+}
---
+++ database/migrations/2021_03_19_000001_create_rates_table.php
@@ -0,0 +1,38 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+class CreateRatesTable extends Migration
+{
+    /**
+     * Run the migrations.
+     *
+     * @return void
+     */
+    public function up()
+    {
+        Schema::create('rates', function (Blueprint $table) {
+            $table->id();
+            $table->unsignedBigInteger('student_id');
+            $table->unsignedBigInteger('lesson_id');
+            $table->integer('rate');
+
+            $table->foreign('student_id')->references('id')->on('users');
+            $table->foreign('lesson_id')->references('id')->on('timetables');
+
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     *
+     * @return void
+     */
+    public function down()
+    {
+        Schema::dropIfExists('rates');
+    }
+}
