58848ab8e1627374bc676bc6cad71e243cbace9c
musiermoore <musiermoore@gmail.com>
2021-05-14 00:00:54 +0600

    Display all users and destroy a user

--- app/Http/Controllers/Api/UserController.php
+++ app/Http/Controllers/Api/UserController.php
@@ -54,4 +54,84 @@ class UserController extends Controller
         ], 200);
     }
 
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function index()
+    {
+        $users = User::with('roles')->get()
+            ->sortBy(function($user) {
+                return $user->roles;
+            });
+
+        return response()->json([
+            'data' => UserResource::collection($users),
+        ])->setStatusCode(200);
+    }
+
+    /**
+     * Store a newly created resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return \Illuminate\Http\Response
+     */
+    public function store(Request $request)
+    {
+        //
+    }
+
+    /**
+     * Display the specified resource.
+     *
+     * @param  \App\Models\User $user
+     * @return \Illuminate\Http\Response
+     */
+    public function show(User $user)
+    {
+        //
+    }
+
+    /**
+     * Update the specified resource in storage.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @param  \App\Models\User $user
+     * @return \Illuminate\Http\Response
+     */
+    public function update(Request $request, User $user)
+    {
+        //
+    }
+
+    /**
+     * Remove the specified resource from storage.
+     *
+     * @param  \App\Models\User  $user
+     * @return \Illuminate\Http\JsonResponse
+     */
+    public function destroy($id)
+    {
+        $user = User::find($id);
+
+        if (empty($user)) {
+            return response()->json([
+                'error' => [
+                    'code'      => 404,
+                    'message'   => "Not found",
+                    'errors'    => "User not found",
+                ],
+            ]);
+        }
+
+        $user->delete();
+
+        return response()->json([
+            'data' => [
+                'code'      => 200,
+                'message'   => 'User was deleted',
+            ],
+        ]);
+    }
 }
